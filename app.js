6948ce2ac5046b386e9776891f538c23992dd9ac
Hunter Huddleston <hunterhuddleston@gmail.com>
2019-05-05 12:48:41 -0500

    Added commentary.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const userScore = 0;
 const computerScore = 0;
 
-let playerSelection = 'ROCK'
+let playerSelection = 'PAPer'
 playerSelection = playerSelection.toLowerCase();
 
 function getComputerChoice() {
@@ -14,19 +14,27 @@ function computerPlay(playerSelection) {
     const computerSelection = getComputerChoice();
     switch (playerSelection + computerSelection) {
         case "rockscissors":
+            console.log("You win! Rock beats scissors.")
+            break;
         case "paperrock":
+            console.log('You win! Paper beats rock.');
+            break;
         case "scissorspaper":
-            console.log("User wins.");
+            console.log("You win! Scissors beats paper.");
             break;
         case "rockpaper":
+            console.log("You lose. Paper beats rock.");
+            break;
         case "paperscissors":
+            console.log('You lose. Scissors beats paper.');
+            break;
         case "scissorsrock":
-            console.log("User loses.")
+            console.log("You lose. Rock beats scissors.");
             break;
         case "rockrock":
         case "paperpaper":
         case "scissorsscissors":
-        console.log("It's a tie.")
+        console.log("It's a draw.")
         break;
     }
 }
